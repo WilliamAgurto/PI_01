63f0e1624ffbcdf55db5290c2f29ff39222bf74e
WilliamAgurto <117483560+WilliamAgurto@users.noreply.github.com>
2023-05-16 03:59:36 -0500

    commit_10

--- FirstApi/main.py
+++ FirstApi/main.py
@@ -17,7 +17,7 @@ async def index():
 
 #loading Data
 path = 'C:\\Users\\Hp\\Documents\\henry course\\Labs\\PI_01\\datasets\\new_movies_dataset.csv'
-df_movies= pd.read_csv(path,parse_dates=['release_date'])
+df_movies= pd.read_csv(path,parse_dates=['release_date'],encoding ='utf-8')
 #df= pd.read_csv('C:\Users\Hp\Documents\henry course\Labs\PI_01\datasets\new_movies_dataset.csv',parse_dates=['release_date'])
 
 
